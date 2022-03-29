40cb4af828f3be3347d399754ff292f033cc28b7
guillermo cala <guillermo.cala7@gmail.com>
2022-03-28 23:26:42 -0500

    ultima mejora para cerradura alfabeto

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def interseccion_listas(lista1, lista2):
             lista_resultado.append(item)
     return lista_resultado
 
-def cerradura_alfabeto(lista, cant_palabras, cant_caracteres):
+def cerradura_alfabeto(lista, cant_palabras):
     lista_resultado = []
     for i in range(cant_palabras):
         palabra = ""
-        for i in range(cant_caracteres):
+        for i in range(random.randint(2, 5)):
             palabra += str(lista[random.randint(0, len(lista) - 1)])
         if palabra not in lista_resultado:
             lista_resultado.append(palabra)
@@ -37,5 +37,4 @@ print("Diferencia(1-2): ", diferencia_listas(lista1, lista2))
 print("Diferencia(2-1): ", diferencia_listas(lista2, lista1))
 print("Interseccion: ", interseccion_listas(lista1, lista2))
 cant_palabras = int(input("Ingrese la cantidad de palabras para la cerradura: "))
-cant_caracteres = int(input("Ingrese la cantidad de caracteres de las palabras: "))
-print("Cerradura: ", cerradura_alfabeto(lista1, cant_palabras, cant_caracteres))
+print("Cerradura: ", cerradura_alfabeto(lista1, cant_palabras))
