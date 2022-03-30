69f20341e7d0f996a2277dd3541c1926a1c75af5
guillermo cala <guillermo.cala7@gmail.com>
2022-03-29 23:17:51 -0500

    falta potencia de lenguaje

--- main.py
+++ main.py
@@ -27,14 +27,32 @@ def cerradura_alfabeto(lista, cant_palabras):
             lista_resultado.append(palabra)
     return lista_resultado
 
+def concatenacion(lista1, lista2):
+    lista_resultado = []
+    for item in lista1:
+        lista_resultado.extend([(str(item) + str(x)) for x in lista2])
+    return lista_resultado
 
-lista1 = [1, 2, 3, 4, 5]
-lista2 = ["a", "e", "i", "o", "u", 1, 2, 3]
+def inversa_lenguaje(lista):
+    lista_resultado = []
+    for i in range(len(lista)):
+        palabra = str(lista[i])
+        lista_resultado.append(palabra[::-1])
+    return lista_resultado
+    
+def cardinal_lista(lista):
+    return len(lista)
+
+lista1 = [123, 234, 345,]
+lista2 = ["a", "e", "i"]
 print("lista1: ", lista1)
 print("lista2: ", lista2)
-print("Union: ", union_listas(lista1, lista2))
+"""print("Union: ", union_listas(lista1, lista2))
 print("Diferencia(1-2): ", diferencia_listas(lista1, lista2))
 print("Diferencia(2-1): ", diferencia_listas(lista2, lista1))
 print("Interseccion: ", interseccion_listas(lista1, lista2))
 cant_palabras = int(input("Ingrese la cantidad de palabras para la cerradura: "))
 print("Cerradura: ", cerradura_alfabeto(lista1, cant_palabras))
+print("Concatenacion: ", concatenacion(lista1, lista2))
+print("Inversa: ", inversa_lenguaje(lista1))"""
+print("Cardinal: ", cardinal_lista(lista1))
